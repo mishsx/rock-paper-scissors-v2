09d154479c1217e44c6c08e75fe9de4fb9580db1
Shubham Mishra <mishrashubhamx@gmail.com>
2020-07-11 16:35:14 -0400

    Added logic to handle game conditions

--- app.js
+++ app.js
@@ -16,8 +16,74 @@ const game = () => {
     });
   };
 
+  //Play match
+  const playMatch = () =>{
+    const options = document.querySelectorAll(".options button")
+    const playerHand = document.querySelector(".player-hand")
+    const computerHand = document.querySelector(".computer-hand")
+    //Computer Options
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach(option=>{
+      option.addEventListener("click", function(){
+        const computerNumber = Math.floor(Math.random()*3);
+        const computerChoice = computerOptions[computerNumber];
+
+
+        //Update images
+        playerHand.src = `./assets/${this.textContent}.png`;
+        computerHand.src = `./assets/${computerChoice}.png`;
+      });
+    });
+  };
+
+const compareHands = (playerChoice, computerChoice) => {
+//Update the text.
+const winner =document.querySelector('.winner');
+//Checking for a tie
+if(playerChoice === computerChoice){
+  winner.textContent = 'It is a tie';
+  return;
+}
+//Check for rock
+ if(playerChoice === 'rock'){
+   if(computerChoice === 'scissors'){
+     winner.textContent = 'Player Wins';
+     return;
+   }else {
+     winner.textContent = 'Computer Wins';
+     return;
+   }
+ }
+//Check for Paper
+if(playerChoice === 'paper'){
+  if(computerChoice === 'rock'){
+    winner.textContent = 'Player Wins';
+    return;
+  }else {
+    winner.textContent = 'Computer Wins';
+    return;
+  }
+}
+//Check for Scissors
+if(playerChoice === 'scissors'){
+  if(computerChoice === 'paper'){
+    winner.textContent = 'Player Wins';
+    return;
+  }else {
+    winner.textContent = 'Computer Wins';
+    return;
+  }
+}
+
+}
+
+
+
+
   // Calling the inner function
   startGame();
+  playMatch();
 };
 
 // initiate the main function
